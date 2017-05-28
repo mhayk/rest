9f506388c00677cba9e09881bab06fa29548765c
Mhayk Whandson <eu@mhayk.com.br>
2017-05-27 20:07:16 -0400

    UsuarioEntity mapped;
    Signed-off-by: Mhayk Whandson <eu@mhayk.com.br>

--- ServicoRest/ServicoRest/UsuarioEntity.cs
+++ ServicoRest/ServicoRest/UsuarioEntity.cs
@@ -1,11 +1,116 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Runtime.Serialization;
 
 namespace ServicoRest
 {
+    [DataContract]
     public class UsuarioEntity
     {
+
+        private Nullable<int> codigo;
+        private String nome;
+        private String login;
+        private String senha;
+        private String tipo;
+        private Boolean registroAtivo;
+        private Int32 codigoSetor;
+
+        [DataMember]
+        public int? Codigo
+        {
+            get
+            {
+                return codigo;
+            }
+
+            set
+            {
+                codigo = value;
+            }
+        }
+
+        [DataMember]
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+
+            set
+            {
+                nome = value;
+            }
+        }
+
+        [DataMember]
+        public string Login
+        {
+            get
+            {
+                return login;
+            }
+
+            set
+            {
+                login = value;
+            }
+        }
+
+        [DataMember]
+        public string Senha
+        {
+            get
+            {
+                return senha;
+            }
+
+            set
+            {
+                senha = value;
+            }
+        }
+
+        [DataMember]
+        public string Tipo
+        {
+            get
+            {
+                return tipo;
+            }
+
+            set
+            {
+                tipo = value;
+            }
+        }
+
+        [DataMember]
+        public bool RegistroAtivo
+        {
+            get
+            {
+                return registroAtivo;
+            }
+
+            set
+            {
+                registroAtivo = value;
+            }
+        }
+
+        [DataMember]
+        public int CodigoSetor
+        {
+            get
+            {
+                return codigoSetor;
+            }
+
+            set
+            {
+                codigoSetor = value;
+            }
+        }
     }
 }
